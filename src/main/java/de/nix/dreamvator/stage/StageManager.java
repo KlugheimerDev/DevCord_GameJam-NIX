44c2b8e9b2940ca8a036360069f89bb25692ff4d
Amirs11 <nick.spam.mar@gmail.com>
2022-04-03 01:56:54 +0200

    Hardcoded New Lobbies

--- src/main/java/de/nix/dreamvator/stage/StageManager.java
+++ src/main/java/de/nix/dreamvator/stage/StageManager.java
@@ -27,16 +27,14 @@ public class StageManager {
     }
 
     public void setCurrentStage(int id) {
-        Dreamvator.getPlayers().get(0).teleport(new Location(Dreamvator.getPlayers().get(0).getWorld(),-13.5, -58, -61, -70, 68));
-        Dreamvator.getPlayers().get(1).teleport(new Location(Dreamvator.getPlayers().get(1).getWorld(),-12.5, -58, -61, 55, 68));
+        Dreamvator.getPlayers().get(0).teleport(new Location(Dreamvator.getPlayers().get(0).getWorld(),currentStage.x1, -58, -61, -70, 68));
+        Dreamvator.getPlayers().get(1).teleport(new Location(Dreamvator.getPlayers().get(1).getWorld(),currentStage.x2, -58, -61, 55, 68));
 
         for(int i = 0; i < 6; i++)
             Dreamvator.getPlayers().get(0).getWorld().playSound(new Location(Dreamvator.getPlayers().get(0).getWorld() ,-15, -59, -60), Sound.BLOCK_BELL_USE, 1, 1);
 
         Stage[] stages = Stage.values();
         currentStage = stages[id];
-
-        Bukkit.broadcastMessage(currentStage.getID() + "");
     }
 
     private void throwEffect() {
@@ -60,12 +58,20 @@ public class StageManager {
     public enum Stage{
         LOBBY(0, -13.5, -12.5),
         STAGE1(1, -13.5, -12.5),
-        STAGE2(2, -13.5, -12.5);
+        STAGE2(2, -53.5, -52.5),
+        STAGE3(3, -92.5, -91.5),
+        STAGE4(4, 83.5, 84.5),
+        STAGE5(5, 33, 34),
+        ENDE(6, -13.5, 12.5);
 
         private final int id;
+        private final double x1;
+        private final double x2;
 
         Stage(int id, double x1, double x2) {
             this.id = id;
+            this.x1 = x1;
+            this.x2 = x2;
         }
 
         public int getID() {
